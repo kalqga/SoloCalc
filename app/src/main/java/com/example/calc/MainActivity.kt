be891483b102dca321982077805ab0ea4cf5f818
mialskywalker <kalqga123@gmail.com>
2021-11-08 12:17:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,171 @@
+package com.example.calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.ArithmeticException
+
+class MainActivity : AppCompatActivity() {
+
+    private var textInput: TextView? = null
+    var lastNum = false
+    var hasDot = false
+    var byZero = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textInput = findViewById(R.id.textView)
+    }
+
+    fun onDigit(view: View){
+        // Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        if (!byZero){
+            textInput?.append((view as Button).text)
+            lastNum = true
+        }
+    }
+
+    fun onBack(view: View){
+        if (!byZero){
+            textInput?.text = textInput?.text?.dropLast(1)
+            if (textInput?.length()!! > 0){
+                lastNum = true
+            }
+        }
+    }
+
+    fun onClear(view: View){
+        textInput?.text = ""
+        byZero = false
+    }
+
+    fun onOperator(view: View){
+        if (!byZero){
+            textInput?.text?.let {
+                if (lastNum && !isOperatorAdded(it.toString())){
+                    textInput?.append((view as Button).text)
+                    lastNum = false
+                    hasDot = false
+                }
+            }
+        }
+    }
+
+    fun onFloat(view: View){
+        if (!byZero)
+        if (lastNum && !hasDot){
+            textInput?.append((view as Button).text)
+            lastNum = false
+            hasDot = true
+        }
+    }
+
+    fun isEqual(view: View){
+        if (!byZero){
+            var result = textInput?.text.toString()
+            var prefix = ""
+
+            try {
+                if(result.startsWith("-")){
+                    prefix = "-"
+                    result = result.substring(1)
+                }
+                if (result.contains("-")){
+                    val splitValue = result.split("-")
+                    var first = splitValue[0]
+                    var second = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        first = prefix + first
+                    }
+
+                    var total = (first.toDouble() - second.toDouble()).toString()
+                    textInput?.text = doubleToInt(total)
+                }else if (result.contains("+")){
+                    val splitValue = result.split("+")
+                    var first = splitValue[0]
+                    var second = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        first = prefix + first
+                    }
+
+                    textInput?.text = (first.toDouble() + second.toDouble()).toString()
+                }else if (result.contains("*")){
+                    val splitValue = result.split("*")
+                    var first = splitValue[0]
+                    var second = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        first = prefix + first
+                    }
+
+                    textInput?.text = (first.toDouble() * second.toDouble()).toString()
+                }else if (result.contains("/")){
+                    val splitValue = result.split("/")
+                    var first = splitValue[0]
+                    var second = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        first = prefix + first
+                    }
+                    // TODO: by zero
+                    if (second.toInt() == 0){
+                        textInput?.text = "Cannot divide by zero"
+                        byZero = true
+                    }else{
+                        textInput?.text = (first.toDouble() / second.toDouble()).toString()
+                    }
+
+                }else if (result.contains("%")){
+                    val splitValue = result.split("%")
+                    var first = splitValue[0]
+                    var second = splitValue[1]
+
+                    if (prefix.isNotEmpty()){
+                        first = prefix + first
+                    }
+                    // TODO: by zero
+                    if (second.toInt() == 0){
+                        textInput?.text = "Cannot % by zero"
+                        byZero = true
+                    }else{
+                        textInput?.text = (first.toDouble() / second.toDouble()).toString()
+                    }
+
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+
+
+    }
+
+    private fun doubleToInt(value: String): String {
+        // TODO: display whole numbers
+
+        if (value.endsWith(".0")){
+            value.dropLast(2)
+            return value
+        }
+        return value
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("%")
+                    || value.contains("/")
+                    || value.contains("*")
+                    || value.contains("-")
+                    || value.contains("+")
+        }
+    }
+}
